2402220351e17b303c30c40d0bc6dd1cabebd3b8
feryndka <feryandika151@gmail.com>
2024-02-21 22:35:08 +0700

    build:add sidebar item

--- src/components/templates/HomeLayout.tsx
+++ src/components/templates/HomeLayout.tsx
@@ -1,37 +1,17 @@
 import React from "react";
 import Navbar from "../organism/Navbar";
 import ButtonItemSidebar from "../atoms/ButtonItemSidebar";
-import { GoHomeFill } from "react-icons/go";
-import { SiYoutubeshorts } from "react-icons/si";
-import { MdSubscriptions } from "react-icons/md";
-
-interface ISidebarItem {
-  icon: React.ReactNode;
-  label: string;
-  isActive?: boolean;
-  href: string;
-}
-
-const SIDEBAR_ITEM: ISidebarItem[] = [
-  {
-    icon: <GoHomeFill size={20} />,
-    label: "Beranda",
-    isActive: true,
-    href: "/",
-  },
-  {
-    icon: <SiYoutubeshorts size={20} />,
-    label: "Shorts",
-    isActive: false,
-    href: "/shorts",
-  },
-  {
-    icon: <MdSubscriptions size={20} />,
-    label: "Subscriptions",
-    isActive: false,
-    href: "/subscriptions",
-  },
-];
+import {
+  SIDEBAR_ITEM_HOME,
+  SIDEBAR_ITEM_ACCOUNT,
+  SIDEBAR_ITEM_EXPLORE,
+  SIDEBAR_ITEM_BROWSE,
+  SIDEBAR_ITEM_MORE,
+  SIDEBAR_ITEM_SETTING
+} from "@/utils/sidebar-item";
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import Button from "../atoms/Button";
+import { GrAddCircle } from "react-icons/gr";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -39,7 +19,8 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
       <Navbar />
       <div className="flex mt-2">
         <div className="w-[15vw] h-[91vh] flex flex-col gap-1 overflow-y-scroll">
-          {SIDEBAR_ITEM.map((item, index) => (
+          {/* HOME */}
+          {SIDEBAR_ITEM_HOME.map((item, index) => (
             <ButtonItemSidebar
               href={item.href}
               key={index}
@@ -49,7 +30,81 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               {item.label}
             </ButtonItemSidebar>
           ))}
-          <div className="h-px bg-white/15 my-3"></div>
+          <div className="h-px bg-white/25 my-3"></div>
+          {/* ACCOUNT */}
+          {SIDEBAR_ITEM_ACCOUNT.map((item, index) => (
+            <ButtonItemSidebar
+              href={item.href}
+              key={index}
+              isActive={item.isActive}
+            >
+              {item.icon}
+              {item.label}
+            </ButtonItemSidebar>
+          ))}
+          <div className="h-px bg-white/25 my-3"></div>
+          {/* SIGN IN */}
+          <div className="mx-5">
+            <div className="mb-3">
+              Sign in to like videos, comment, and subscribe.
+            </div>
+            <Button>
+              <HiOutlineUserCircle size={30} />
+              Sign In
+            </Button>
+          </div>
+          <div className="h-px bg-white/25 my-3"></div>
+          {/* Explore */}
+          <h1 className="font-semibold mx-5 text-md text-md mb-1">Explore</h1>
+          {SIDEBAR_ITEM_EXPLORE.map((item, index) => (
+            <ButtonItemSidebar
+              href={item.href}
+              key={index}
+              isActive={item.isActive}
+            >
+              {item.icon}
+              {item.label}
+            </ButtonItemSidebar>
+          ))}
+          <div className="h-px bg-white/25 my-3"></div>
+          {/* BROWSE CHANNELS */}
+          {SIDEBAR_ITEM_BROWSE.map((item, index) => (
+            <ButtonItemSidebar
+              href={item.href}
+              key={index}
+              isActive={item.isActive}
+            >
+              {item.icon}
+              {item.label}
+            </ButtonItemSidebar>
+          ))}
+          <div className="h-px bg-white/25 my-3"></div>
+          {/* MORE */}
+          <h1 className="font-semibold mx-5 text-md text-md mb-1">
+            More from YouTube
+          </h1>
+          {SIDEBAR_ITEM_MORE.map((item, index) => (
+            <ButtonItemSidebar
+              href={item.href}
+              key={index}
+              isActive={item.isActive}
+            >
+              {item.icon}
+              {item.label}
+            </ButtonItemSidebar>
+          ))}
+          <div className="h-px bg-white/25 my-3"></div>
+          {/* SETTING */}
+          {SIDEBAR_ITEM_SETTING.map((item, index) => (
+            <ButtonItemSidebar
+              href={item.href}
+              key={index}
+              isActive={item.isActive}
+            >
+              {item.icon}
+              {item.label}
+            </ButtonItemSidebar>
+          ))}
         </div>
         <div>{children}</div>
       </div>
---
+++ src/utils/sidebar-item.tsx
@@ -0,0 +1,155 @@
+import { GoHomeFill, GoTrophy } from "react-icons/go";
+import {
+  SiYoutubeshorts,
+  SiYoutubegaming,
+  SiYoutubemusic,
+} from "react-icons/si";
+import { MdSubscriptions, MdOutlineVideoLibrary, MdOutlineOutlinedFlag } from "react-icons/md";
+import { GrAddCircle, GrHistory } from "react-icons/gr";
+import { BsFire } from "react-icons/bs";
+import { PiMusicNoteLight } from "react-icons/pi";
+import { BiMovie, BiNews } from "react-icons/bi";
+import { FaYoutube } from "react-icons/fa";
+import { TbBrandYoutubeKids } from "react-icons/tb";
+import { IoSettingsOutline } from "react-icons/io5";
+import { VscReport } from "react-icons/vsc";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+
+interface ISidebarItem {
+  icon: React.ReactNode;
+  label: string;
+  isActive?: boolean;
+  href: string;
+}
+
+export const SIDEBAR_ITEM_HOME: ISidebarItem[] = [
+  {
+    icon: <GoHomeFill size={20} />,
+    label: "Home",
+    isActive: true,
+    href: "/",
+  },
+  {
+    icon: <SiYoutubeshorts size={20} />,
+    label: "Shorts",
+    isActive: false,
+    href: "/shorts",
+  },
+  {
+    icon: <MdSubscriptions size={20} />,
+    label: "Subscriptions",
+    isActive: false,
+    href: "/subscriptions",
+  },
+];
+
+export const SIDEBAR_ITEM_ACCOUNT: ISidebarItem[] = [
+  {
+    icon: <MdOutlineVideoLibrary size={20} />,
+    label: "You",
+    isActive: false,
+    href: "/you",
+  },
+  {
+    icon: <GrHistory size={20} />,
+    label: "History",
+    isActive: false,
+    href: "/history",
+  },
+];
+
+export const SIDEBAR_ITEM_EXPLORE: ISidebarItem[] = [
+  {
+    icon: <BsFire size={20} />,
+    label: "Trending",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <PiMusicNoteLight size={20} />,
+    label: "Music",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <BiMovie size={20} />,
+    label: "Movies",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <SiYoutubegaming size={20} />,
+    label: "Gaming",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <BiNews size={20} />,
+    label: "News",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <GoTrophy size={20} />,
+    label: "Sports",
+    isActive: false,
+    href: "#",
+  },
+];
+
+export const SIDEBAR_ITEM_BROWSE: ISidebarItem[] = [
+  {
+    icon: <GrAddCircle size={20} />,
+    label: "Browse channels",
+    isActive: false,
+    href: "#",
+  },
+];
+
+export const SIDEBAR_ITEM_MORE: ISidebarItem[] = [
+  {
+    icon: <FaYoutube size={20} style={{ color: "red"}} />,
+    label: "Youtube Premium",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <SiYoutubemusic size={20} style={{ color: "red" }} />,
+    label: "Youtube Music",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <TbBrandYoutubeKids size={20} style={{ color: "red" }} />,
+    label: "Youtube Kids",
+    isActive: false,
+    href: "#",
+  },
+];
+
+export const SIDEBAR_ITEM_SETTING: ISidebarItem[] = [
+  {
+    icon: <IoSettingsOutline size={20} />,
+    label: "Settings",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <MdOutlineOutlinedFlag size={20} />,
+    label: "Report history",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <IoIosHelpCircleOutline size={20} />,
+    label: "Help",
+    isActive: false,
+    href: "#",
+  },
+  {
+    icon: <VscReport size={20} />,
+    label: "Send feedback",
+    isActive: false,
+    href: "#",
+  },
+];

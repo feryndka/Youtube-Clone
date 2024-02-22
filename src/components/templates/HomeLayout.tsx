99d0b7a670f26e0e9bae7cf87435d1f989ee9b4d
feryndka <feryandika151@gmail.com>
2024-02-22 17:06:25 +0700

    build:add footer sidebar

--- src/components/templates/HomeLayout.tsx
+++ src/components/templates/HomeLayout.tsx
@@ -7,11 +7,10 @@ import {
   SIDEBAR_ITEM_EXPLORE,
   SIDEBAR_ITEM_BROWSE,
   SIDEBAR_ITEM_MORE,
-  SIDEBAR_ITEM_SETTING
+  SIDEBAR_ITEM_SETTING,
 } from "@/utils/sidebar-item";
 import { HiOutlineUserCircle } from "react-icons/hi2";
 import Button from "../atoms/Button";
-import { GrAddCircle } from "react-icons/gr";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -30,7 +29,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               {item.label}
             </ButtonItemSidebar>
           ))}
-          <div className="h-px bg-white/25 my-3"></div>
+          <div className="h-px bg-white/25 my-2"></div>
           {/* ACCOUNT */}
           {SIDEBAR_ITEM_ACCOUNT.map((item, index) => (
             <ButtonItemSidebar
@@ -42,7 +41,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               {item.label}
             </ButtonItemSidebar>
           ))}
-          <div className="h-px bg-white/25 my-3"></div>
+          <div className="h-px bg-white/25 my-2"></div>
           {/* SIGN IN */}
           <div className="mx-5">
             <div className="mb-3">
@@ -53,7 +52,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               Sign In
             </Button>
           </div>
-          <div className="h-px bg-white/25 my-3"></div>
+          <div className="h-px bg-white/25 my-2"></div>
           {/* Explore */}
           <h1 className="font-semibold mx-5 text-md text-md mb-1">Explore</h1>
           {SIDEBAR_ITEM_EXPLORE.map((item, index) => (
@@ -66,7 +65,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               {item.label}
             </ButtonItemSidebar>
           ))}
-          <div className="h-px bg-white/25 my-3"></div>
+          <div className="h-px bg-white/25 my-2"></div>
           {/* BROWSE CHANNELS */}
           {SIDEBAR_ITEM_BROWSE.map((item, index) => (
             <ButtonItemSidebar
@@ -78,7 +77,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               {item.label}
             </ButtonItemSidebar>
           ))}
-          <div className="h-px bg-white/25 my-3"></div>
+          <div className="h-px bg-white/25 my-2"></div>
           {/* MORE */}
           <h1 className="font-semibold mx-5 text-md text-md mb-1">
             More from YouTube
@@ -93,7 +92,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               {item.label}
             </ButtonItemSidebar>
           ))}
-          <div className="h-px bg-white/25 my-3"></div>
+          <div className="h-px bg-white/25 my-2"></div>
           {/* SETTING */}
           {SIDEBAR_ITEM_SETTING.map((item, index) => (
             <ButtonItemSidebar
@@ -105,6 +104,16 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
               {item.label}
             </ButtonItemSidebar>
           ))}
+          <div className="h-px bg-white/25 my-2"></div>
+          <div className="flex flex-col gap-3 ms-5 me-4 mb-5">
+            <div className="text-sm text-white/60 font-semibold">
+              About Press Copyright Contact us Creators Advertise Developers
+            </div>
+            <div className="text-sm text-white/60 font-semibold">
+              Terms Privacy Policy & Safety How YouTube works <br /> Test new features
+            </div>
+            <div className="text-sm text-white/40">© 2024 Google LLC</div>
+          </div>
         </div>
         <div>{children}</div>
       </div>
